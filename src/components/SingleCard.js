d8b54d3e0b2091390e327fb7a8ff2ce2c6049235
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-07-14 16:03:57 +0530

    ${process.env.PUBLIC_URL}

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,21 +1,21 @@
 import './SingleCard.css'
 
-export default function SingleCard({card,handleChoice,flipped,disabled}) {
-  const handleClick=()=>{
-    if(!disabled){
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
       handleChoice(card)
     }
   }
-    return (
-        <div className="card">
-          <div className={flipped? 'flipped':''}>
-            <img className='front' src={card.src} alt='Front of Card'/>
-            <img className='back' 
-            src='/img/cover.png' 
-            alt='Back of Card'
-            onClick={handleClick}
-            />
-          </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <div className={flipped ? 'flipped' : ''}>
+        <img className='front' src={card.src} alt='Front of Card' />
+        <img className='back'
+          src={`${process.env.PUBLIC_URL}/img/cover.png`}
+          alt='Back of Card'
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  )
 }
